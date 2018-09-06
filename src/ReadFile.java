54c63b7a1b8add46d258e605b490e76db2c4b49d
jackieboynom <jack141094@gmail.com>
2018-09-06 13:18:13 -0500

    finished parsing config file

--- src/ReadFile.java
+++ src/ReadFile.java
@@ -4,19 +4,18 @@ import java.io.File;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ReadFile {
 
-    public static class nodes {
+    public static class Nodes {
         int nodeID;
-        int portNumber;
         String hostName;
-        int [] nodalConnections;
+        int portNumber;
+        ArrayList <Integer> nodalConnections = new ArrayList<Integer>();
     }
-
+    
     public static void main(String[] args) {
-        //Declare Variables
-
         try {
             //create variables
             File file = new File("src/config_file.txt");
@@ -25,31 +24,76 @@ public class ReadFile {
             StringBuffer stringBuffer = new StringBuffer();
             String line;
             int numNode;
+            boolean empty = false;
+            int valid_lines = 0;
 
             //read in number of nodes
             numNode = Integer.parseInt(bufferedReader.readLine());
+            //create array to hold pointers to nodes
+            Nodes [] array_of_nodes = new Nodes[numNode];
 
-            //read in node info
-            while ((line = bufferedReader.readLine()) != null) {
-                if (!line.isEmpty()) {
-                    if (line.contains("#")) {
-                        int index = line.indexOf("#");
-                        line = line.substring(0, index);
-                    }
-                    line = line.trim();
-                    System.out.println(line);
-
+            //node info
+            while (valid_lines != numNode) {
+                line = bufferedReader.readLine();
+                empty = false;
+                //format each line to correct format
+                if (line.contains("#")) {
+                    int index = line.indexOf("#");
+                    line = line.substring(0, index);
+                }
+                if (line.isEmpty()) {
+                    empty = true;
+                }
+                //create nodes
+                if (!empty) {
+                    //parse info
+                    Nodes node = new Nodes();
+                    node.nodeID = Integer.parseInt(line.substring(0, 1));
+                    node.hostName = line.substring(2, 19);
+                    node.portNumber = Integer.parseInt(line.substring(20, 24));
+                    array_of_nodes[valid_lines] = node;
+                    valid_lines++;
                 }
             }
-            //fileReader.close();
-            //System.out.println("Contents of file:");
-            //System.out.println(stringBuffer.toString());
+            //nodal connections
+            valid_lines = 0;
+            while (valid_lines != numNode) {
+                line = bufferedReader.readLine();
+                empty = false;
+                //format each line to correct format
+                if (line.contains("#")) {
+                    int index = line.indexOf("#");
+                    line = line.substring(0, index);
+                }
+                if (line.isEmpty()) {
+                    empty = true;
+                }
+                //create array to store nodal connections
+                if (!empty) {
+                    int nodeId = Integer.parseInt(line.substring(0, 1));
+                    line = line.substring(2);
+                    while (line != null) {
+                        //parse info
+                        int next;
+                        System.out.println("1" + line);
+                        if (!(line.indexOf(" ") == -1)) {
+                            next = line.indexOf(" ");
+                            array_of_nodes[nodeId].nodalConnections.add(Integer.parseInt(line.substring(0, next)));
+                            line = line.substring(next+1);
+                        } else {
+                            next = 1;
+                            array_of_nodes[nodeId].nodalConnections.add(Integer.parseInt(line.substring(0, next)));
+                            line = null;
+                        }
+                    }
+                    valid_lines++;
+                }
+            }
+            for (int i = 0; i < numNode; i++) {
+                System.out.println(array_of_nodes[i].nodalConnections);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
-
-
-
 }
