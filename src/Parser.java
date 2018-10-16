fe9564933a64a21403bd5c136046b439f0c3d371
Ko-Chen Chen <jack141094@gmail.com>
2018-10-16 11:27:30 -0500

    Update Parser.java

--- src/Parser.java
+++ src/Parser.java
@@ -6,15 +6,6 @@ import java.util.ArrayList;
 
 public class Parser {
     public static Nodes[] parse (String PATH) {
-        //open file
-        try {
-            File file = new File(PATH);
-            FileReader fileReader = new FileReader(file);
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         //get number of nodes
         int numNode = 0;
         try {
@@ -22,25 +13,8 @@ public class Parser {
             File file = new File(PATH);
             FileReader fileReader = new FileReader(file);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            String line = bufferedReader.readLine();
-            int index;
-            boolean empty;
-            int valid_lines = 0;
-
-            while (valid_lines != 1) {
-                empty = false;
-                if (line.contains("#")) {
-                    index = line.indexOf("#");
-                    line = line.substring(0, index);
-                }
-                if (line.isEmpty()) {
-                    empty = true;
-                }
-                if (!empty) {
-                    numNode = Integer.parseInt(bufferedReader.readLine());
-                    bufferedReader.close();
-                }
-            }
+            numNode = Integer.parseInt(bufferedReader.readLine());
+            bufferedReader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -77,29 +51,53 @@ public class Parser {
                     //parse info
                     Nodes node = new Nodes();
                     node.setNodalConnections(numNode);
-                    node.setNodeID(valid_lines + 1);
                     index = line.indexOf(" ");
-                    node.setHostName(line.substring(0, index) + "utdallas.edu");
+                    node.setNodeID(Integer.parseInt(line.substring(0, index)));
+                    line = line.substring(index).trim();
+                    index = line.indexOf(" ");
+                    node.setHostName(line.substring(0, index));
                     line = line.substring(index).trim();
                     node.setPortNumber(Integer.parseInt(line.substring(0, 4)));
-                    line = line.substring(index).trim();
                     array_of_nodes[valid_lines] = node;
+                    valid_lines++;
+                }
+            }
+            //nodal connections
+            valid_lines = 0;
+            while (valid_lines != numNode) {
+                line = bufferedReader.readLine();
+                empty = false;
+                //format each line to correct format
+                if (line.contains("#")) {
+                    int index = line.indexOf("#");
+                    line = line.substring(0, index);
+                }
+                if (line.isEmpty()) {
+                    empty = true;
+                }
+                line = line.trim();
+                //create array to store nodal connections
+                if (!empty) {
+                    int next = line.indexOf(" ");
+                    int nodeId = Integer.parseInt(line.substring(0, next));
+                    line = line.substring(next+1);
                     while (line != null) {
                         //parse info
-                        int next;
+                        next = 0;
                         if (line.contains(" ")) {
                             next = line.indexOf(" ");
                             ArrayList<Integer> path = new ArrayList<>();
-                            path.add(valid_lines + 1);
+                            path.add(nodeId);
+                            System.out.print(line.substring(0, next));
                             path.add(Integer.parseInt(line.substring(0, next)));
-                            array_of_nodes[valid_lines + 1].addNodalConnections(path, Integer.parseInt(line.substring(0, next)));
+                            array_of_nodes[nodeId].addNodalConnections(path, Integer.parseInt(line.substring(0, next)));
                             line = line.substring(next+1);
                         } else {
-                            next = 1;
+                            //next = 1;
                             ArrayList<Integer> path = new ArrayList<>();
-                            path.add(valid_lines + 1);
-                            path.add(Integer.parseInt(line.substring(0, next)));
-                            array_of_nodes[valid_lines + 1].addNodalConnections(path, Integer.parseInt(line.substring(0, next)));
+                            path.add(nodeId);
+                            path.add(Integer.parseInt(line));
+                            array_of_nodes[nodeId].addNodalConnections(path, Integer.parseInt(line));
                             line = null;
                         }
                     }
